94817267ac0d6040e80ab7300ac1d38847875fe9
Hamza-Mahmood-3811 <hamzamahmood9151@gmail.com>
2023-10-06 17:47:44 -0400

    Setting up. Added Favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center">Hello Tailwind CSS!</h1>
+    <div className="">
+      <h1 className="text-3xl font-bold underline text-center">Hello Tailwind CSS!</h1>
+      <h2 className="text-center text-[50px]">Custom CSS</h2>
+
+      {/* Layout */}
+    </div>
   )
 }
