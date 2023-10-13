fa1d562fa077649f3ecef981ea1f7aa11603b0ec
Hamza-Mahmood-3811 <hamzamahmood9151@gmail.com>
2023-10-12 21:02:18 -0400

    started outlining the Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Heroo</div>
+    <section id='home' className='w-full border-2  border-red-500 p-2 flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'>
+      <div className=''></div>
+    </section>
   )
 }
 
