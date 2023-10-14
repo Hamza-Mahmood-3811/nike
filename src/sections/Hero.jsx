9ec00f21d17c552e92ffbd3a85d56b907a8858e7
Hamza-Mahmood-3811 <hamzamahmood9151@gmail.com>
2023-10-14 13:51:39 -0400

    fixing hero section design bug

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
     <section id='home' className='w-full border-2  border-red-500 p-2 flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'>
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-coral-red'>Our Summer Collection</p>
-        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold'>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
           <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival</span>
           <br />
           <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
@@ -28,8 +28,8 @@ const Hero = () => {
           ) )}
         </div>
       </div>
-      <div className='relative flex-1 flex-container justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center '>
-        <img src={bigShoe1} alt="shoe collection" width={610} height={500} className='object-contain relative z-10 mt-20' />
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center '>
+        <img src={bigShoe1} alt="shoe collection" width={610} height={500} className='object-contain relative z-10' />
         <div className=''>
           {shoes.map( (shoe) => (
             <div key={shoe}>
