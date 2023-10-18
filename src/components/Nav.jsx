49bd5b0b99578590e7206914f3675ea17b336959
Hamza-Mahmood-3811 <hamzamahmood9151@gmail.com>
2023-10-18 01:38:42 -0400

    Fixing tailwind CSS styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,8 +6,7 @@ import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
-    // add 'bg-pale-blue' to header class and change 'absolute' to 'fixed'
-    <header className='padding-x py-8 absolute z-10 w-full'>
+    <header className='padding-x py-8 fixed z-20 w-full bg-pale-blue'>
       <nav className='flex justify-between items-center max-container'>
         <a href="/">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
